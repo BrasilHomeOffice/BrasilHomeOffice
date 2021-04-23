a14c9f9eeaaa810f8b46a349e062c65529756a05
Giovanne Afonso <giovanneafonso@gmail.com>
2021-04-23 13:59:49 -0300

    Update tf-apply

--- scripts/tf-apply.sh
+++ scripts/tf-apply.sh
@@ -3,5 +3,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 source $DIR/utils/variables.sh
 
 cd $TF_DIR
-terraform apply -var-file="./config.tfvars"
+terraform apply -var-file="./config.tfvars" --auto-approve
 cd $DIR
